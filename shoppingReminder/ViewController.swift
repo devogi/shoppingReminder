e39ace1dad38c800b492655e4b3ca6779d1113b7
Oguzhan <ogigogi56@gmail.com>
2023-01-09 23:05:51 +0300

    Initial Commit

---
+++ shoppingReminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shoppingReminder
+//
+//  Created by oğuzhan salkım on 9.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
